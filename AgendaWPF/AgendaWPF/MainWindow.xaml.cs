78e489f7ae5b10d82c526de7f5d99bf808fea359
Helena Nunes <helenanc@uol.com.br>
2016-11-17 14:12:32 -0300

    CRUD - OK HTTP - NO

--- AgendaWPF/AgendaWPF/MainWindow.xaml.cs
+++ AgendaWPF/AgendaWPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,82 @@ namespace AgendaWPF
         public MainWindow()
         {
             InitializeComponent();
+            compromissos = new List<Models.Compromisso>();
+        }
+
+        private string ip = "http://localhost:53097";
+        private List<Models.Compromisso> compromissos;
+
+        private bool IsRealizado()
+        {
+            if (checkBox.IsChecked == true) return true;
+            else return false;
+        }
+
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            Models.Compromisso comp = new Models.Compromisso();
+            comp.Descricao = txtDesc.Text;
+            comp.Local = txtLocal.Text;
+            comp.Data = datepicker.SelectedDate.Value;
+            comp.Realizado = IsRealizado();
+            compromissos.Add(comp);
+            MessageBox.Show("Cadastrado!");
+        }
+
+        private void btnListar_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Models.Compromisso c in compromissos)
+            {
+                lvCompromissos.Items.Add("Desc: " + c.Descricao + 
+                    " Local: " + c.Local + " Data: " + c.Data.Date + 
+                    " Realizado: " + c.Realizado);
+            }
+        }
+
+        /* HTTP */
+
+        private async void btnDispServ_Click(object sender, RoutedEventArgs e)
+        {
+            //faz contato com o servidor
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+
+            //entra no servidor e pega uma lista de compromissos
+            var response = await httpClient.GetAsync("/api/agenda/");
+            var str = response.Content.ReadAsStringAsync().Result;
+            List<Models.Compromisso> obj = JsonConvert.DeserializeObject<List<Models.Compromisso>>(str);
+
+            //deleta todos os compromissos do servidor de acordo com a lista obtida
+            foreach (Models.Compromisso c in obj)
+                await httpClient.DeleteAsync("/api/agenda/" + c.Id.ToString());
+
+            //pega a lista do dispositivo, serializa e insere no servidor
+            string s = "=" + JsonConvert.SerializeObject(compromissos);
+            var content = new StringContent(s, Encoding.UTF8, "application/x-www-form-urlencoded");
+            await httpClient.PostAsync("/api/agenda/", content);
+
+            MessageBox.Show("Atualizado (D-S)!");
+        }
+
+        private async void btnServDisp_Click(object sender, RoutedEventArgs e)
+        {
+            // Acessa os dados do serviço para recuperar a lista de compromissos
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(ip);
+            var response = await httpClient.GetAsync("/api/agenda/");
+            var str = response.Content.ReadAsStringAsync().Result;
+
+            // Converte o json do serviço para uma lista
+            List<Models.Compromisso> obj = JsonConvert.DeserializeObject<List<Models.Compromisso>>(str);
+
+            // Apaga os dados locais
+            compromissos.RemoveRange(0, compromissos.Count);
+
+            // Grava a lista de compromissos no dispositivo;
+            compromissos = obj;
+
+            MessageBox.Show("Atualizado (S-D)!");
         }
     }
 }
